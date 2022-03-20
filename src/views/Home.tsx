07dd7963f0f128a143a634d84b278a2d70ad45af
Melmoth <marcos.mon.rod@gmail.com>
2022-03-20 21:50:47 +0100

    feat: edit view Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-
+import { ProductCard } from "../components/ProductCard";
+import { products } from "../db"
+import { useSelector } from "react-redux";
 export const Home = () => {
+  const productList = useSelector((state: any)=>state.products.value)
   return (
     <div className="container">
       <h1>Home</h1>
+      {
+          productList.map((v: any, i: number)=>(
+            <ProductCard
+                key={i}
+                name={v.name}
+                description={v.description}
+                img={v.img}
+                price={v.price}
+            />
+          ))
+      }
     </div>
   );
 };
